22c9b2cac93996f90001689c8efe70a41c9af681
dankata7 <d.vladimirov2017@pmg-pernik.com>
2021-10-21 12:41:37 +0300

    Add new project

---
+++ Manufactors/Manufactors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Manufactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
